32b5d7bb81a04c8748ffdf7bc1ac7d07bd1b2d90
Dmitriy Voicu <voiku173dmitry@gmail.com>
2023-06-11 20:06:08 +0300

    project initialisation

---
+++ src/main/java/com/planty_app/Planty/PlantyApplication.java
@@ -0,0 +1,13 @@
+package com.planty_app.Planty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantyApplication.class, args);
+	}
+
+}
